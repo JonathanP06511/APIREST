d0d15fa21a41f21fbaf29e22ea9cb38bd168eee4
Jonathan Chasipanta <jonathan.paul065@gmail.com>
2024-05-26 15:51:20 -0500

    update server.js

--- server/server.js
+++ server/server.js
@@ -3,8 +3,7 @@ const bodyParser = require('body-parser');
 const itemsRoutes = require('./routes/itemsRoutes');
 
 const app = express();
-const port = 3000;
-
+const port = process.env.PORT || 3000; 
 app.use(bodyParser.json());
 
 app.use('/api/items', itemsRoutes);
